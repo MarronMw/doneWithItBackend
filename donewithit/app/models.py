027fdb407faba3a946a9ee1bd57e56e643184470
marandege <marandege@gmail.com>
2025-06-06 14:30:22 +0200

    Verbose and default str on listings

--- donewithit/app/models.py
+++ donewithit/app/models.py
@@ -1,8 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+
 class Listings(models.Model):
-    title=models.CharField(name="title")
-    price=models.IntegerField(name="price")
-    image=models.ImageField(name="image",upload_to='./uploads')
-        
+    title = models.CharField(name="title")
+    price = models.IntegerField(name="price")
+    image = models.ImageField(name="image", upload_to='./uploads')
+
+    def __str__(self):
+        return self.title+" - $" + str(self.price)
+
+    class Meta:
+        verbose_name = "Listing"
+        verbose_name_plural = "Listings"
+        ordering = ['-id']
