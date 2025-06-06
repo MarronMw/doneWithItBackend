5c4d0518048a77b7a9a570145ccaf45bc2242c2a
marandege <marandege@gmail.com>
2025-06-06 14:40:53 +0200

    MEDIA configurations

--- donewithit/app/views.py
+++ donewithit/app/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 
+from .models import Listings
+
 # Create your views here.
+
+
 def trial(request):
-    return render(request,"index.html")
+    listings = Listings.objects.all()
+    context = {"listings": listings}
+    return render(request, "index.html", context)
--- donewithit/doneWithIt/urls.py
+++ donewithit/doneWithIt/urls.py
@@ -1,19 +1,23 @@
-from django.contrib import admin
-from django.urls import path,include
-from rest_framework import routers
 from app.viewsets import ListingViewSet
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework import routers
 
-#defining the default Api urls and the routes
-router=routers.DefaultRouter()
+# defining the default Api urls and the routes
+router = routers.DefaultRouter()
 
-#add or register api routes here
-router.register(r'listings',ListingViewSet)
-
+# add or register api routes here
+router.register(r'listings', ListingViewSet)
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('ap/', include('rest_framework.urls')),
-    path('api/',include(router.urls)),
-    path('',include('app.urls'))
+    path('api/', include(router.urls)),
+    path('', include('app.urls'))
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
